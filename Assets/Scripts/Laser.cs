53818c7d6b34340d6874146fce588b12a39413b3
Mats <53276381+Matsdd@users.noreply.github.com>
2024-02-08 23:22:27 +0100

    Roller implemented & laser sprite added

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class textScript : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
